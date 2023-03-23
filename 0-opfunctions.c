3e5234ad50cee3c333a58662711b8482367ea1c0
root <livyakendikiruja@gmail.com>
2023-03-23 13:36:11 -0700

    Free stack to clean up

--- 0-opfunctions.c
+++ 0-opfunctions.c
@@ -87,7 +87,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		free_stack(*stack);
+		cleanup(*stack);
 		exit(EXIT_FAILURE);
 	}
 
