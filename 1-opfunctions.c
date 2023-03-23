1150df9f7d169e2d25e26fe025537675898585ad
root <livyakendikiruja@gmail.com>
2023-03-23 14:52:37 -0700

    Implement div opcode

--- 1-opfunctions.c
+++ 1-opfunctions.c
@@ -54,3 +54,31 @@ void sub(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+
+/**
+ * divide - divides the second top element by the top element of the stack
+ * @stack: pointer to the stack
+ * @line_number: the current line number
+ */
+void divide(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		cleanup(*stack);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		cleanup(*stack);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	tmp->n /= (*stack)->n;
+	*stack = tmp;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
--- monty.h
+++ monty.h
@@ -66,6 +66,7 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 
 void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
 void cleanup(stack_t *stack);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 int is_number(char *str);
